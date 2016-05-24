959672b75bd88e467a9026630e51cae0d0c082fa
willriggins <williamriggins2013@gmail.com>
2016-05-24 00:41:52 -0400

    homework

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -5,6 +5,32 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void createItem(Scanner scanner, ArrayList<InventoryItem> items) {
+        System.out.println("Enter new item name to add to the list.");
+        String text = scanner.nextLine();
+        int quantity = 0;
+        InventoryItem item = new InventoryItem(text, quantity);
+        items.add(item);
+        System.out.println(text + " added to inventory.");
+    }
+
+    public static void removeItem(Scanner scanner, ArrayList<InventoryItem> items) {
+        System.out.println("Enter item number you wish to remove from the list.");
+        String intStr = scanner.nextLine();
+        int num = Integer.valueOf(intStr);
+        items.remove(num - 1);
+    }
+
+    public static void updateItem(Scanner scanner, ArrayList<InventoryItem> items) {
+        System.out.println("Which item number do you wish to modify?");
+        String updateNum = scanner.nextLine();
+        int newNum = Integer.valueOf(updateNum);
+        System.out.println("Update with new quantity:");
+        String itemNum = scanner.nextLine();
+        InventoryItem tempItem = items.get(newNum -1);
+        tempItem.quantity += Integer.valueOf(itemNum);
+
+    }
 
     public static void main(String[] args) {
         ArrayList<InventoryItem> items = new ArrayList<>();
@@ -27,31 +53,15 @@ public class Main {
 
             switch (option) {
                 case "1":
-                    System.out.println("Enter new item name to add to the list.");
-                    String text = scanner.nextLine();
-                    InventoryItem item = new InventoryItem(text, 0);
-                    items.add(item);
-                    System.out.println(text + " added to inventory.");
+                    createItem(scanner, items);
                     break;
 
-
                 case "2":
-                    System.out.println("Enter item number you wish to remove from the list.");
-                    String intStr = scanner.nextLine();
-                    int num = Integer.valueOf(intStr);
-                    items.remove(num - 1);
+                    removeItem(scanner, items);
                     break;
 
                 case "3":
-                    System.out.println("Which item number do you wish to modify?");
-                    String intStr2 = scanner.nextLine();
-                    int num2 = Integer.valueOf(intStr2);
-                    num2 -= 1;
-                    System.out.println("Add how many?");
-                    String changeStr = scanner.nextLine();
-                    int change = Integer.valueOf(changeStr);
-
-
+                    updateItem(scanner,items);
                     break;
 
                 default:
